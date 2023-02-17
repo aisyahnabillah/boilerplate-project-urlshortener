ec6486c1a41df82ff1fda6cf3ebcd71f5d5ecd0e
Aisyah Nabillah <aisyahnabillah97@gmail.com>
2023-02-17 21:46:07 +0700

    adding url shortener

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const validUrl = require('valid-url');
+const bodyParser = require('body-parser');
+const shortid = require('shortid');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+
+// Set up body-parser to handle POST request data
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 
@@ -14,9 +20,37 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+// Keep track of shortened URLs in memory
+const urls = {};
+
+// POST endpoint to create a new short URL
+app.post('/api/shorturl', function(req, res) {
+  const { url } = req.body;
+  if (validUrl.isWebUri(url)) {
+    const shortCode = shortid.generate();
+    urls[shortCode] = url;
+    res.json({
+      original_url: url,
+      short_url: shortCode
+    });
+  } else {
+    res.json({
+      error: 'invalid url'
+    });
+  }
+});
+
+// GET endpoint to redirect to the original URL
+app.get('/api/shorturl/:code', function(req, res) {
+  const { code } = req.params;
+  const url = urls[code];
+  if (url) {
+    res.redirect(url);
+  } else {
+    res.json({
+      error: 'invalid short code'
+    });
+  }
 });
 
 app.listen(port, function() {
